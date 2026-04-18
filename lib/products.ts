e18fe5a437401b788a9d4e6e4686cfd0490a354f
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-19 01:44:14 +0200

    Fix product names and update collection IDs

--- lib/products.ts
+++ lib/products.ts
@@ -1,7 +1,7 @@
 export const COLLECTIONS = [
   {
     id: 'urban-fossils',
-    name: 'URBAN FOSSILS',
+    name: 'Urban Fossils',
     products: [
       { name: 'Charcoal Black Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19f852d887productimage699c03358e5fa.jpg' },
       { name: 'Ice Blue Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
@@ -10,33 +10,29 @@ export const COLLECTIONS = [
   },
   {
     id: 'colours-explosion',
-    name: 'COLOURS EXPLOSION',
+    name: 'Colours Explosion',
     products: [
-      { name: 'Maverick Phoenix Hoodie', price: '1400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
-      { name: 'Spectrum Explosion Hoodie', price: '1400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' },
-      { name: 'Maverick Phoenix Set (White Edition)', price: '2400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69aa5b1956607productimage69a4f7b8adae6.png' },
-      { name: 'Maverick Phoenix Set (Black Edition)', price: '2400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' }
+      { name: 'Maverick Phoenix Set - WHITE', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69aa5b1956607productimage69a4f7b8adae6.png' },
+      { name: 'Maverick Phoenix Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
+      { name: 'Maverick Phoenix Set - BLACK', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' },
+      { name: 'Spectrum Explosion Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' }
+    ]
+  },
+  {
+    id: 'aero-collection',
+    name: 'AERO Collection',
+    products: [
+      { name: 'Eternity Protocol - White Base', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg' },
+      { name: 'Eternity Protocol - Navy Blue', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482142676productimage69e14759939d1.jpg' }
     ]
   },
   {
     id: 'light-gear',
-    name: 'LIGHT GEAR',
+    name: 'Light Gear',
     products: [
-      { name: 'Black Flux Zip Hoodie', price: '1300 - 1700', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
-      { name: 'Black Flux Sweatpants', price: '900 - 1400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699d9bbb7e5f1productimage699c03358e601.jpg' },
-      { name: 'Maverick Code Zip Hoodie', price: '1400 - 1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c57e9557ffproductimage699c03358e603.jpg' },
-      { name: 'Light Code Sweatpants', price: '900 - 1300', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699d9be7c3129productimage699c03358e605.jpg' }
-    ]
-  },
-  {
-    id: 'manifesto',
-    name: 'MANIFESTO',
-    products: [
-      { name: 'The Sovereign Hoodie', price: '1500 - 1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b54062cb422productimage6995933b34e69.png' },
-      { name: 'The Anthem Hoodie', price: '1500 - 1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19e7dbdea7productimage699c03358e5ed.jpg' },
-      { name: 'The Decree Hoodie', price: '1500 - 1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0cd8ca5e1cproductimage699c03358e5f2.jpg' },
-      { name: 'High Tide Hoodie', price: '1400 - 1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c4ff03868cproductimage699c03358e5f5.jpg' },
-      { name: 'Blue Authority Hoodie', price: '1400 - 1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c508680247productimage699c03358e5f7.jpg' }
+      { name: 'Black Flux Zip Hoodie', price: '1300', oldPrice: '1700', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
+      { name: 'Black Flux Sweatpants', price: '900', oldPrice: '1400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699d9bbb7e5f1productimage699c03358e601.jpg' },
+      { name: 'Maverick Code Zip Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c57e9557ffproductimage699c03358e603.jpg' }
     ]
   }
 ];
