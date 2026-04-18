3e6f3d19f7d792376a4fe89d689902d782cd325f
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-19 01:54:25 +0200

    Update product collections and details

--- lib/products.ts
+++ lib/products.ts
@@ -1,44 +1,63 @@
 export const COLLECTIONS = [
   {
-    id: 'urban-fossils',
-    name: 'Urban Fossils',
+    id: 'aero-collection',
+    name: 'AERO COLLECTION',
     products: [
-      { name: 'Charcoal Black Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19f852d887productimage699c03358e5fa.jpg' },
-      { name: 'Ice Blue Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
-      { name: 'Dust Rose Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
+      { 
+        name: 'ETERNITY PROTOCOL', 
+        sku: 'ARO-OTOW-EP16',
+        price: '730',
+        image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg',
+        variants: [
+          { colorName: 'White Base', url: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg' },
+          { colorName: 'Navy Blue', url: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69e1482142676productimage69e14759939d1.jpg' }
+        ]
+      }
+    ]
+  },
+  {
+    id: 'urban-fossils',
+    name: 'URBAN FOSSILS',
+    products: [
+      { name: 'Charcoal Black Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c551d958e6productimage699c03358e5fa.jpg' },
+      { name: 'Ice Blue Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
+      { name: 'Dust Rose Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
     ]
   },
   {
     id: 'colours-explosion',
-    name: 'Colours Explosion',
+    name: 'COLOURS EXPLOSION',
     products: [
-      { name: 'Maverick Phoenix Set - WHITE', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69aa5b1956607productimage69a4f7b8adae6.png' },
-      { name: 'Maverick Phoenix Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
-      { name: 'Maverick Phoenix Set - BLACK', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' },
-      { name: 'Spectrum Explosion Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' }
-    ]
-  },
-  {
-    id: 'aero-collection',
-    name: 'AERO Collection',
-    products: [
-      { name: 'Eternity Protocol - White Base', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg' },
-      { name: 'Eternity Protocol - Navy Blue', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482142676productimage69e14759939d1.jpg' }
+      { name: 'Maverick Phoenix Set - WHITE', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69aa674aae5f0productimage69a4f7b8adae6.jpg' },
+      { name: 'Maverick Phoenix Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
+      { name: 'Maverick Phoenix Set - BLACK', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' },
+      { name: 'Spectrum Explosion Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69aa6a7926906productimage699c03358e5f8.jpg' }
     ]
   },
   {
     id: 'light-gear',
-    name: 'Light Gear',
+    name: 'LIGHT GEAR',
     products: [
-      { name: 'Black Flux Zip Hoodie', price: '1300', oldPrice: '1700', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
-      { name: 'Black Flux Sweatpants', price: '900', oldPrice: '1400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699d9bbb7e5f1productimage699c03358e601.jpg' },
-      { name: 'Maverick Code Zip Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c57e9557ffproductimage699c03358e603.jpg' }
+      { name: 'Black Flux Zip Hoodie', price: '1300', oldPrice: '1700', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
+      { name: 'Black Flux Sweatpants', price: '900', oldPrice: '1400', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699d9bbbb2f5aproductimage699c03358e601.jpg' },
+      { name: 'Maverick Code Zip Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c57e9557ffproductimage699c03358e603.jpg' },
+      { name: 'Light Code Sweatpants', price: '900', oldPrice: '1300', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699d9be7c3129productimage699c03358e605.jpg' }
+    ]
+  },
+  {
+    id: 'manifesto',
+    name: 'MANIFESTO',
+    products: [
+      { name: 'The Sovereign Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b54062cb422productimage6995933b34e69.png' },
+      { name: 'The Anthem Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b19e7dbdea7productimage699c03358e5ed.jpg' },
+      { name: 'The Decree Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b0cd8ca5e1cproductimage699c03358e5f2.jpg' },
+      { name: 'High Tide Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b0cb334f851productimage699c03358e5f5.jpg' },
+      { name: 'Blue Authority Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c508680247productimage699c03358e5f7.jpg' }
     ]
   }
 ];
 
 export const CONTACT_LINKS = {
   whatsapp: "https://api.whatsapp.com/send?phone=201033866838",
-  instagram: "https://www.instagram.com/swaymaverick",
-  facebook: "https://www.facebook.com/profile.php?id=61587128586975"
+  instagram: "https://www.instagram.com/swaymaverick"
 };
