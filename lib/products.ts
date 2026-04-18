bd0507a98b89fe4c660ac8763906466c56897e33
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-18 19:09:53 +0200

    Create products.ts

---
+++ lib/products.ts
@@ -0,0 +1,26 @@
+export const COLLECTIONS = [
+  {
+    id: 'urban-fossils',
+    name: 'Urban Fossils',
+    products: [
+      { name: 'Charcoal Black Rogue Relic Hoodie', price: 1500, oldPrice: 1900, image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-null,h-null//products/696e0bcb55259/69b19f852d887productimage699c03358e5fa.jpg' },
+      { name: 'Ice Blue Rogue Relic Hoodie', price: 1500, oldPrice: 1900, image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-null,h-null//products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
+      { name: 'Dust Rose Rogue Relic Hoodie', price: 1500, oldPrice: 1900, image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-null,h-null//products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
+    ]
+  },
+  {
+    id: 'colours-explosion',
+    name: 'Colours Explosion',
+    products: [
+      { name: 'Maverick Phoenix Hoodie', price: 1400, image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-null,h-null//products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
+      { name: 'Spectrum Explosion Hoodie', price: 1400, image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-null,h-null//products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' }
+    ]
+  }
+  // Add Manifesto and Light Gear here following the same pattern
+];
+
+export const CONTACT_LINKS = {
+  whatsapp: "https://api.whatsapp.com/send?phone=201033866838",
+  instagram: "https://www.instagram.com/swaymaverick",
+  facebook: "https://www.facebook.com/profile.php?id=61587128586975"
+};
