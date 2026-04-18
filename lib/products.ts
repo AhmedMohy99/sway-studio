6e58212fade6f7581ccd56c47a29e27718974ed8
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-19 01:20:03 +0200

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,11 +1,27 @@
 export const COLLECTIONS = [
   {
+    id: 'aero-collection',
+    name: 'AERO COLLECTION',
+    products: [
+      { 
+        name: 'ETERNITY PROTOCOL', 
+        sku: 'ARO-OTOW-EP16',
+        price: '730',
+        image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg',
+        variants: [
+          { color: 'White Base', img: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg' },
+          { color: 'Navy Blue', img: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482142676productimage69e14759939d1.jpg' }
+        ]
+      }
+    ]
+  },
+  {
     id: 'urban-fossils',
     name: 'URBAN FOSSILS',
     products: [
-      { name: 'CHARCOAL BLACK ROGUE RELIC HOODIE', price: '1,500', oldPrice: '1,900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19f852d887productimage699c03358e5fa.jpg' },
-      { name: 'ICE BLUE ROGUE RELIC HOODIE', price: '1,500', oldPrice: '1,900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
-      { name: 'DUST ROSE ROGUE RELIC HOODIE', price: '1,500', oldPrice: '1,900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
+      { name: 'CHARCOAL BLACK ROGUE RELIC HOODIE', price: '1,500', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19f852d887productimage699c03358e5fa.jpg' },
+      { name: 'ICE BLUE ROGUE RELIC HOODIE', price: '1,500', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
+      { name: 'DUST ROSE ROGUE RELIC HOODIE', price: '1,500', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
     ]
   },
   {
@@ -13,41 +29,12 @@ export const COLLECTIONS = [
     name: 'COLOURS EXPLOSION',
     products: [
       { name: 'MAVERICK PHOENIX HOODIE', price: '1,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
-      { name: 'SPECTRUM EXPLOSION HOODIE', price: '1,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' },
-      { name: 'MAVERICK PHOENIX SET (WHITE)', price: '2,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69aa5b1956607productimage69a4f7b8adae6.png' },
-      { name: 'MAVERICK PHOENIX SET (BLACK)', price: '2,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' }
-    ]
-  },
-  {
-    id: 'aero-collection',
-    name: 'AERO COLLECTION',
-    products: [
-      { name: 'ETERNITY PROTOCOL - WHITE BASE', sku: 'ARO-OTOW-EP16-WHT', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482147a27productimage69e14759939d1.jpg' },
-      { name: 'ETERNITY PROTOCOL - NAVY BLUE', sku: 'ARO-OTOW-EP16-NVY', price: '730', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1000/products/696e0bcb55259/69e1482142676productimage69e14759939d1.jpg' }
-    ]
-  },
-  {
-    id: 'light-gear',
-    name: 'LIGHT GEAR',
-    products: [
-      { name: 'BLACK FLUX ZIP HOODIE', price: '1,300 - 1,700', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
-      { name: 'BLACK FLUX SWEATPANTS', price: '900 - 1,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/699d9bbb7e5f1productimage699c03358e601.jpg' }
-    ]
-  },
-  {
-    id: 'manifesto',
-    name: 'MANIFESTO',
-    products: [
-      { name: 'THE SOVEREIGN HOODIE', price: '1,500 - 1,900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b54062cb422productimage6995933b34e69.png' },
-      { name: 'THE ANTHEM HOODIE', price: '1,500 - 1,900', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b19e7dbdea7productimage699c03358e5ed.jpg' }
+      { name: 'SPECTRUM EXPLOSION HOODIE', price: '1,400', image: 'https://ik.imagekit.io/8tgwppr1x/tr:w-1000,h-1000/products/696e0bcb55259/69b0a37015ff1productimage699c03358e5f8.jpg' }
     ]
   }
 ];
 
-export const MANIFESTO_TEXT = "Sway Maverick rejects the ordinary, mastering the art of scarcity through unique drops and rare silhouettes. Local, Loud, and Limitless.";
-
 export const CONTACT_LINKS = {
   whatsapp: "https://api.whatsapp.com/send?phone=201033866838",
-  instagram: "https://www.instagram.com/swaymaverick",
-  facebook: "https://www.facebook.com/profile.php?id=61587128586975"
+  instagram: "https://www.instagram.com/swaymaverick"
 };
