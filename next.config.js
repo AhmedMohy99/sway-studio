e688420495be3c0bb3dcdc28ca1090159ee4b196
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-19 00:47:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+    ],
   },
 }
 
