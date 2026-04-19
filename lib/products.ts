2927b48792beafb15b82924c373c62127992490b
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-19 02:12:41 +0200

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -19,35 +19,17 @@ export const COLLECTIONS = [
     id: 'urban-fossils',
     name: 'URBAN FOSSILS',
     products: [
-      { name: 'Charcoal Black Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c551d958e6productimage699c03358e5fa.jpg' },
-      { name: 'Ice Blue Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
-      { name: 'Dust Rose Rogue Relic Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
+      { name: 'Charcoal Black Rogue Relic', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c551d958e6productimage699c03358e5fa.jpg' },
+      { name: 'Ice Blue Rogue Relic', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c55ab75572productimage699c03358e5fc.jpg' },
+      { name: 'Dust Rose Rogue Relic', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c5654e26ecproductimage699c03358e5fe.jpg' }
     ]
   },
   {
     id: 'colours-explosion',
     name: 'COLOURS EXPLOSION',
     products: [
-      { name: 'Maverick Phoenix Set - WHITE EDITION', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69aa674aae5f0productimage69a4f7b8adae6.jpg' },
-      { name: 'Maverick Phoenix Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' },
-      { name: 'Maverick Phoenix Set', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69a4eb6cc4c0dproductimage69a4eb2b29665.jpg' },
-      { name: 'Spectrum Explosion Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69aa6a7926906productimage699c03358e5f8.jpg' }
-    ]
-  },
-  {
-    id: 'light-gear',
-    name: 'LIGHT GEAR',
-    products: [
-      { name: 'Black Flux Zip Hoodie', price: '1300', oldPrice: '1700', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699c56c3913bcproductimage699c03358e600.jpg' },
-      { name: 'Black Flux Sweatpants', price: '900', oldPrice: '1400', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/699d9bbbb2f5aproductimage699c03358e601.jpg' }
-    ]
-  },
-  {
-    id: 'manifesto',
-    name: 'MANIFESTO',
-    products: [
-      { name: 'The Sovereign Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b54062cb422productimage6995933b34e69.png' },
-      { name: 'The Anthem Hoodie', price: '1500', oldPrice: '1900', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69b19e7dbdea7productimage699c03358e5ed.jpg' }
+      { name: 'Maverick Phoenix Set - White', price: '2400', oldPrice: '2800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69aa674aae5f0productimage69a4f7b8adae6.jpg' },
+      { name: 'Maverick Phoenix Hoodie', price: '1400', oldPrice: '1800', image: 'https://ik.imagekit.io/uiumblazm/tr:w-1000,h-1500/products/696e0bcb55259/69ac482151071productimage69a5d9cc657ab.jpg' }
     ]
   }
 ];
