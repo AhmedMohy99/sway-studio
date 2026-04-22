ee03a38052fc4b663175a63d5efe3766b08cefe3
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-22 16:14:51 +0200

    Fix tryOnImage paths for product images
    Updated tryOnImage paths to include an additional '.png' extension for several products.

--- lib/products.ts
+++ lib/products.ts
@@ -1,3 +1,5 @@
+// lib/products.ts
+
 export type SizeOption = 'S' | 'M' | 'L' | 'XL' | '2XL'
 
 export type Product = {
@@ -9,8 +11,6 @@ export type Product = {
   variants?: { colorName: string; tryOnImage: string }[]
 }
 
-export const SIZES: SizeOption[] = ['S', 'M', 'L', 'XL', '2XL']
-
 export const CONTACT_LINKS = {
   whatsapp: 'https://api.whatsapp.com/send?phone=201033866838',
 }
@@ -21,35 +21,35 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     price: '440.00',
     type: 'regular',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6b889925deproductimage69e6b51508e46.jpg',
-    tryOnImage: '/powder-blue-venture-tee.png',
+    tryOnImage: '/powder-blue-venture-tee.png.png',
   },
   {
     name: 'The Catalyst Tee',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6b1f0dd14cproductimage69e6b1aa693e8.jpg',
-    tryOnImage: '/catalyst-tee.png',
+    tryOnImage: '/catalyst-tee.png.png',
   },
   {
     name: 'Bluish Splash',
     price: '730.00',
     type: 'regular',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6af828f618productimage69e6af182e668.jpg',
-    tryOnImage: '/bluish-splash.png',
+    tryOnImage: '/bluish-splash.png.png',
   },
   {
     name: 'Greenish Splash',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6aa596ae5cproductimage69e6a931d9611.jpg',
-    tryOnImage: '/greenish-splash.png',
+    tryOnImage: '/greenish-splash.png.png',
   },
   {
     name: 'Yellowish Splash',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6ad3d902f4productimage69e6ac753e18a.jpg',
-    tryOnImage: '/yellowish-splash.png',
+    tryOnImage: '/yellowish-splash.png.png',
   },
   {
     name: 'The Maverick Phoenix',
@@ -57,8 +57,8 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e69abfcec21productimage69e69a06054c4.jpg',
     variants: [
-      { colorName: 'Dark Black', tryOnImage: '/maverick-phoenix-black.png' },
-      { colorName: 'White Base', tryOnImage: '/maverick-phoenix-white.png' },
+      { colorName: 'Dark Black', tryOnImage: '/maverick-phoenix-black.png.png' },
+      { colorName: 'White Base', tryOnImage: '/maverick-phoenix-white.png.png' },
     ],
   },
   {
@@ -66,6 +66,6 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6a659a24b7productimage69e6a59ddf53b.jpg',
-    tryOnImage: '/cyber-crescent.png',
+    tryOnImage: '/cyber-crescent.png.png',
   }
 ]
