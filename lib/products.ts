31fed94c02603d4fa2395525d63ffe54850b4b0f
Ahmed Mohy <64763461+AhmedMohy99@users.noreply.github.com>
2026-04-22 16:26:08 +0200

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -21,35 +21,35 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     price: '440.00',
     type: 'regular',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6b889925deproductimage69e6b51508e46.jpg',
-    tryOnImage: '/powder-blue-venture-tee.png.png',
+    tryOnImage: '/powder-blue-venture-tee.png',
   },
   {
     name: 'The Catalyst Tee',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6b1f0dd14cproductimage69e6b1aa693e8.jpg',
-    tryOnImage: '/catalyst-tee.png.png',
+    tryOnImage: '/catalyst-tee.png',
   },
   {
     name: 'Bluish Splash',
     price: '730.00',
     type: 'regular',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6af828f618productimage69e6af182e668.jpg',
-    tryOnImage: '/bluish-splash.png.png',
+    tryOnImage: '/bluish-splash.png',
   },
   {
     name: 'Greenish Splash',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6aa596ae5cproductimage69e6a931d9611.jpg',
-    tryOnImage: '/greenish-splash.png.png',
+    tryOnImage: '/greenish-splash.png',
   },
   {
     name: 'Yellowish Splash',
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6ad3d902f4productimage69e6ac753e18a.jpg',
-    tryOnImage: '/yellowish-splash.png.png',
+    tryOnImage: '/yellowish-splash.png',
   },
   {
     name: 'The Maverick Phoenix',
@@ -57,8 +57,8 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e69abfcec21productimage69e69a06054c4.jpg',
     variants: [
-      { colorName: 'Dark Black', tryOnImage: '/maverick-phoenix-black.png.png' },
-      { colorName: 'White Base', tryOnImage: '/maverick-phoenix-white.png.png' },
+      { colorName: 'Dark Black', tryOnImage: '/maverick-phoenix-black.png' },
+      { colorName: 'White Base', tryOnImage: '/maverick-phoenix-white.png' },
     ],
   },
   {
@@ -66,6 +66,17 @@ export const TRY_TEST_PRODUCTS: Product[] = [
     price: '730.00',
     type: 'oversized',
     image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e6a659a24b7productimage69e6a59ddf53b.jpg',
-    tryOnImage: '/cyber-crescent.png.png',
+    tryOnImage: '/cyber-crescent.png',
   }
 ]
+
+export const PREORDER_PRODUCT: Product = {
+  name: 'Eternity Protocol',
+  price: '730.00',
+  type: 'oversized',
+  image: 'https://ik.imagekit.io/5yvgym2qm/tr:w-1000,h-1500/products/696e0bcb55259/69e14821423d8productimage69e14759939d1.jpg',
+  variants: [
+    { colorName: 'White', tryOnImage: '/eternity-protocol-white.png' },
+    { colorName: 'Navy Blue', tryOnImage: '/eternity-protocol-navy.png' },
+  ],
+}
